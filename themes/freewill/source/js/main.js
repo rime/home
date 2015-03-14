719d45566be94e99d98d867f50e588a97df055c7
lotem <chen.sst@gmail.com>
2015-03-15 00:17:30 +0800

    front matter.

--- themes/freewill/source/js/main.js
+++ themes/freewill/source/js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 
 	$(window).scroll(function(){  //As long as the window to scroll to trigger the code below
         var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //Get height after rolling
-        if( scrollt >200 ){  //Height exceeds the judgment scroll 200px, on display
+        if( scrollt > 40 ){  //Height exceeds the judgment scroll, on display
             $("#gotop").fadeIn(333); //Fade
 			$(".navbar").stop().fadeTo(333, 0.2);
         }else{
             $("#gotop").fadeOut(333); //If the return or does not exceed, on a fade. Must add stop () stops before the movie, there would be flashing
-			$(".navbar").stop().fadeTo(333, 1);
+			$(".navbar").stop().fadeTo(0, 1);
         }
     });
     $("#gotop").click(function(){ //When you click on the label when using animate within 200 milliseconds, rolled top
