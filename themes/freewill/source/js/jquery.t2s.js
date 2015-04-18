444ff326ae3bdd4112858cbc4378b5601cadc3fb
lotem <chen.sst@gmail.com>
2015-04-18 14:59:39 +0800

    skeleton t2s library.

---
+++ themes/freewill/source/js/jquery.t2s.js
@@ -0,0 +1,196 @@
+﻿/**
+ * jquery.t2s.js
+ *
+ * A Traditional Chinese to Simplified Chinese conversion library based on jquery-s2t and OpenCC data.
+ * Copyright Rime Developers
+ * ---
+ * jquery-s2t v0.1.0
+ *
+ * https://github.com/hustlzp/jquery-s2t
+ * A jQuery plugin to convert between Simplified Chinese and Traditional Chinese.
+ * Tested in IE6+, Chrome, Firefox.
+ *
+ * Copyright 2013-2014 hustlzp
+ * Released under the MIT license
+ */
+
+(function($) {
+
+    /**
+     * 简体字
+     * @const
+     */
+    var S = '';
+
+    /**
+     * 繁体字
+     * @const
+     */
+    var T = '';
+
+    /**
+     * 转换文本
+     * @param {String} str - 待转换的文本
+     * @param {Boolean} toT - 是否转换成繁体
+     * @returns {String} - 转换结果
+     */
+    function tranStr(str, toT) {
+        var i;
+        var letter;
+        var code;
+        var isChinese;
+        var index;
+        var src, des;
+        var result = '';
+
+        if (toT) {
+            src = S;
+            des = T;
+        } else {
+            src = T;
+            des = S;
+        }
+
+        if (typeof str !== "string") {
+            return str;
+        }
+
+        for (i = 0; i < str.length; i++) {
+            letter = str.charAt(i);
+            code = str.charCodeAt(i);
+
+            // 根据字符的Unicode判断是否为汉字，以提高性能
+            // 参考:
+            // [1] http://www.unicode.org
+            // [2] http://zh.wikipedia.org/wiki/Unicode%E5%AD%97%E7%AC%A6%E5%88%97%E8%A1%A8
+            // [3] http://xylonwang.iteye.com/blog/519552
+            isChinese = (code > 0x3400 && code < 0x9FC3) || (code > 0xF900 && code < 0xFA6A);
+
+            if (!isChinese) {
+                result += letter;
+                continue;
+            }
+
+            index = src.indexOf(letter);
+
+            if (index !== -1) {
+                result += des.charAt(index);
+            } else {
+                result += letter;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 转换HTML Element属性
+     * @param {Element} element - 待转换的HTML Element节点
+     * @param {String|Array} attr - 待转换的属性/属性列表
+     * @param {Boolean} toT - 是否转换成繁体
+     */
+    function tranAttr(element, attr, toT) {
+        var i, attrValue;
+
+        if (attr instanceof Array) {
+            for(i = 0; i < attr.length; i++) {
+                tranAttr(element, attr[i], toT);
+            }
+        } else {
+            attrValue = element.getAttribute(attr);
+
+            if (attrValue !== "" && attrValue !== null) {
+                element.setAttribute(attr, tranStr(attrValue, toT));
+            }
+        }
+    }
+
+    /**
+     * 转换HTML Element节点
+     * @param {Element} element - 待转换的HTML Element节点
+     * @param {Boolean} toT - 是否转换成繁体
+     */
+    function tranElement(element, toT) {
+        var i;
+        var childNodes;
+
+        if (element.nodeType !== 1) {
+            return;
+        }
+
+        childNodes = element.childNodes;
+
+        for (i = 0; i < childNodes.length; i++) {
+            var childNode = childNodes.item(i);
+
+            // 若为HTML Element节点
+            if (childNode.nodeType === 1) {
+                // 对以下标签不做处理
+                if ("|BR|HR|TEXTAREA|SCRIPT|OBJECT|EMBED|".indexOf("|" + childNode.tagName + "|") !== -1) {
+                    continue;
+                }
+
+                tranAttr(childNode, ['title', 'data-original-title', 'alt', 'placeholder'], toT);
+
+                // input 标签
+                // 对text类型的input输入框不做处理
+                if (childNode.tagName === "INPUT"
+                    && childNode.value !== ""
+                    && childNode.type !== "text"
+                    && childNode.type !== "hidden")
+                {
+                    childNode.value = tranStr(childNode.value, toT);
+                }
+
+                // 继续递归调用
+                tranElement(childNode, toT);
+            } else if (childNode.nodeType === 3) {  // 若为文本节点
+                childNode.data = tranStr(childNode.data, toT);
+            }
+        }
+    }
+
+    // 扩展jQuery全局方法
+    $.extend({
+        /**
+         * 文本简转繁
+         * @param {String} str - 待转换的文本
+         * @returns {String} 转换结果
+         */
+        s2t: function(str) {
+            return tranStr(str, true);
+        },
+
+        /**
+         * 文本繁转简
+         * @param {String} str - 待转换的文本
+         * @returns {String} 转换结果
+         */
+        t2s: function(str) {
+            return tranStr(str, false);
+        }
+    });
+
+    // 扩展jQuery对象方法
+    $.fn.extend({
+        /**
+         * jQuery Objects简转繁
+         * @this {jQuery Objects} 待转换的jQuery Objects
+         */
+        s2t: function() {
+            return this.each(function() {
+                tranElement(this, true);
+            });
+        },
+
+        /**
+         * jQuery Objects繁转简
+         * @this {jQuery Objects} 待转换的jQuery Objects
+         */
+        t2s: function() {
+            return this.each(function() {
+                tranElement(this, false);
+            });
+        }
+    });
+}) (jQuery);
