a0b6f35c63715377a8527bc6ce18dcd4ecbe30ce
Chen Gong <chen.sst@gmail.com>
2018-05-19 19:28:49 +0800

    feat(notice): display notice for matching time period; implements rip-mode

---
+++ blog/themes/freewill/source/js/notice.js
@@ -0,0 +1,31 @@
+const notices = [
+  {
+    start: '2018-05-19 GMT+0800',
+    length: 7,
+    mode: 'rip-mode',
+    notice: '鄭張尚芳先生千古'
+  },
+];
+
+(function maybeDisplayNotice() {
+  let today = new Date();
+  for (let entry of notices) {
+    let startDate = new Date(entry.start);
+    if (today < startDate) {
+      continue;
+    }
+    let endDate = startDate;
+    endDate.setDate(endDate.getDate() + entry.length);
+    if (today > endDate) {
+      continue;
+    }
+    if (entry.mode) {
+      $(document.body).addClass(entry.mode);
+    }
+    if (entry.notice) {
+      $('.front-matter .slogan').hide();
+      $('.front-matter .notice').text(entry.notice);
+    }
+    return;
+  }
+})();
